95c209c49454a3b274a7bd3f7ae23776edd96cbd
JackMarx <seriousfools@gmail.com>
2018-01-08 21:06:10 -0600

    attr_reader/attr_writer

--- employee.rb
+++ employee.rb
@@ -14,31 +14,14 @@
 
 
 class Employee
+  attr_reader :first_name, :last_name, :salary, :active
+  attr_writer :active
+
   def initialize(input_first_name, input_last_name, input_salary, input_active)
     @first_name = input_first_name
     @last_name = input_last_name
     @salary = input_salary
     @active = input_active
-  end
-
-  def first_name
-    @first_name
-  end
-
-  def last_name
-    @last_name
-  end
-
-  def salary
-    @salary
-  end
-
-  def active
-    @active
-  end
-
-  def active=(new_active)
-    @active = new_active
   end
 
   def print_info
@@ -52,6 +35,7 @@ end
 
 employee_1 = Employee.new("Bruce", "Wayne", 70000, true)
 employee_2 = Employee.new("Diana", "Prince", 80000, true)
+employee_2.print_info
 p employee_1.active
 employee_1.active = false
 p employee_1.active
