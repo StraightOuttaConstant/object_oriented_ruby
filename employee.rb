6270ff478bc9940e371b5be497e79d5f607491a0
JackMarx <seriousfools@gmail.com>
2018-01-09 20:15:58 -0600

    fire everyone

--- employee.rb
+++ employee.rb
@@ -1,18 +1,3 @@
-
-# employee_1 = ["Bruce", "Wayne", 70000, true]
-# employee_2 = ["Diana", "Prince", 80000, true]
-
-# puts employee_1[0] + " " + employee_1[1] + " makes " + employee_1[2].to_s + " a year."
-# puts "#{employee_2[0]} #{employee_2[1]} makes #{employee_2[2]} a year."
-
-
-# employee_1 = {:first_name => "Bruce", :last_name => "Wayne", :salary => 70000, :active => true}
-# employee_2 = {first_name: "Diana", last_name: "Prince", salary: 80000, active: true}
-
-# puts "#{employee_1[:first_name]} #{employee_1[:last_name]} makes #{employee_1[:salary]} a year."
-# puts "#{employee_2[:first_name]} #{employee_2[:last_name]} makes #{employee_2[:salary]} a year."
-
-
 class Employee
   attr_reader :first_name, :last_name, :salary, :active
   attr_writer :active
@@ -46,16 +31,33 @@ employee_2 = Employee.new(
                           salary: 80000, 
                           active: true
                           )
-employee_2.print_info
-p employee_1.active
-employee_1.active = false
-p employee_1.active
+# employee_2.print_info
+# p employee_1.active
+# employee_1.active = false
+# p employee_1.active
 
 class Manager < Employee
+  def initialize(input_options)
+    super(input_options)
+    @employees = input_options[:employees]
+  end
+
   def send_report
     puts "Sending Email..."
     #code that sends email
     puts "Email sent."
+  end
+
+  def give_all_raises
+    @employees.each do |employee|
+      employee.give_annual_raise
+    end
+  end
+
+  def fire_all_employees
+    @employees.each do |employee|
+      employee.active = false
+    end
   end
 end
 
@@ -63,17 +65,14 @@ manager = Manager.new(
                       first_name: "Clark",
                       last_name: "Kent",
                       salary: 100000,
-                      active: true
+                      active: true,
+                      employees: [employee_1, employee_2]
                       )
-manager.print_info
-manager.send_report
-
-
-
-
-
-
-
+p employee_1.active
+p employee_2.active
+manager.fire_all_employees
+p employee_1.active
+p employee_2.active
 
 
 
